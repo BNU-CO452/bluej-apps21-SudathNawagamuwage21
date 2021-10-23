b89dfac5b0bfe2d5b1a6c6547b7623d9d2201324
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2021-10-23 10:28:37 +0100

    add author name to student

--- app212-grades/Student.java
+++ app212-grades/Student.java
@@ -4,8 +4,8 @@ import java.util.*;
  * It holds the student details relevant in our context.
  * 
  * @author Michael Kölling and David Barnes
- * Modified by Derek Peacock & Nicholas Day
- * @version 2021-08-18
+ * Modified by Sudath Nawagamuwage
+ * @version 2021-10-23
  */
 public class Student
 {
