813732ecb30504281d07d88a1bd20d3e9110f034
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2021-10-29 00:58:57 +0100

    create the  modules as set

--- app212-grades/Course.java
+++ app212-grades/Course.java
@@ -44,8 +44,16 @@ public class Course
      */
     public void createModules()
     {
-
-    }
+        Module co452 = new Module ("CO452", "Programming Concepts");
+        Module co450 = new Module ("CO450", "Computer Architectures");
+        Module co456 = new Module ("CO456", "Web Development");
+        Module co455 = new Module ("CO455", "DTPP");
+        addModule(co452);
+        addModule(co450);
+        addModule(co456);
+        addModule(co455);
+    
+    }   
     
     public void addModule(Module module)
     {
@@ -60,7 +68,18 @@ public class Course
      */
     public Grades convertToGrade(int mark)
     {
-        return Grades.NS;
+        if(mark >= 0 && mark <= 39)
+            return Grades.F;
+        else if(mark >= 40 && mark <=49)
+            return Grades.D;
+        else if (mark >= 50 && mark <=59)
+            return Grades.C;
+        else if(mark >= 60 && mark <= 69)
+            return Grades.B;
+        else if(mark >= 70 && mark <= 100)
+            return Grades.A;
+        else
+            return Grades.NS;
     }
     
     /**
@@ -69,12 +88,25 @@ public class Course
      */
     public Grades calculateGrade(ArrayList<ModuleMark> marks)
     {
-        return Grades.NS;
+        int total = 0;
+        int finalMark = 0;
+        
+        for (ModuleMark mark : marks)
+        {
+            total = total + mark . getValue();
+        }
+        
+        finalMark = total / MAXN_MODULES;
+        finalGrade = convertToGrade(finalMark);
+        
+        return finalGrade;
+        
+        //return Grades.NS;
     }
     
     /**
      * Prints out the details of a course and the
-     * four modules
+     * four modules. 
      */
     public void print()
     {
@@ -86,7 +118,7 @@ public class Course
     }
     
     /**
-     * Print the course's four modules
+     * Print the course's four modules and the credit
      */
     public void printModules()
     {
