9e0c375703fe599b628f5cc7af2c6362cc6ea718
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2021-12-04 11:22:15 +0000

    Create main class for app to run

--- app216-zuul-test-inteillj/src/com/company/Main.java
+++ app216-zuul-test-inteillj/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
+/**
+ * Write a description of class Program here.
+ *
+ * @modified By Sudath Nawagamuwage
+ * @version 04/12/2021
+ */
 
 public class Main {
 
+    private static Game app;
+
     public static void main(String[] args) {
+
+        app = new Game();
+        app.play();
+
+
 	// write your code here
     }
 }
